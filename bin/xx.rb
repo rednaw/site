6c3a55c6a3e476d15c99a65c33ceb94517855fa8
wander <w.grevink@sig.eu>
2021-04-08 17:38:33 +0200

    Site updated

---
+++ bin/xx.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+require 'bundler/inline'
+require 'yaml'
+require 'pp'
+
+gemfile do
+  source 'https://rubygems.org'
+  gem 'bibtex-ruby', '~> 6'
+  gem 'nokogiri', '~> 1.11'
+  gem 'text', '~> 1.3'
+end
+
+def add(bib, entry)
+  entry.delete('date-added')
+  entry.delete('date-modified')
+  bib << entry
+  bib
+end
+
+#:parse_names parser option to false.
+#BibTeX.parse(<<-END).to_xml(:extended => true)
+
+bib = BibTeX.open(ARGV[0], :parse_names => false)
+# saved = BibTeX::Bibliography.new
+# bib.each { |bib_entry|
+#   if !bib_entry.author.nil? && bib_entry.author.downcase.include?('aloni')
+#     saved = add(saved, bib_entry)
+#   end
+# }
+# saved.save_to('aloni_web.bib',  :parse_names => false)
+
+
+
+# html_entries.each { |html_entry|
+#   puts html_entry
+#   closest = bib_entries.map { |bib_entry|
+#     {
+#       'value' => bib_entry,
+#       'distance' => Text::Levenshtein.distance(html_entry, bib_entry)
+#     }
+#   }.sort_by { |hsh| hsh['distance'] }.first['value']
+#   puts '--'
+#   puts closest
+#   puts
+#   puts
+# }
+# html = Nokogiri::HTML(File.read('../articles.html'))
+# html_entries = html.xpath('//li/a').map { |node|
+#   node.text
+# }
--- content/publications.md
+++ content/publications.md
@@ -9,10 +9,16 @@ permalink: /publications
 {% bibliography -f content/aloni.bib --query @article]%}
 
 <h4>Conference papers</h4>
-{% bibliography -f content/aloni.bib --query @inproceedings --sort_by year%}
+{% bibliography -f content/aloni.bib --query @conference || @inproceedings %}
 
 <h4>Book chapters</h4>
 {% bibliography -f content/aloni.bib --query @incollection %}
+
+<h4>Drafts</h4>
+{% bibliography -f content/aloni.bib --query @unpublished %}
+
+<h4>PHD thesis</h4>
+{% bibliography -f content/aloni.bib --query @phdthesis %}
 
 
 
